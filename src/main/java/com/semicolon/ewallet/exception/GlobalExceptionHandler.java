7744ec08f1ac4b15139409e239169763ad977158
Tade Emmanuel <tadeemmanuel08@gmail.com>
2023-04-27 12:32:42 +0100

    new commit

--- src/main/java/com/semicolon/ewallet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/semicolon/ewallet/exception/GlobalExceptionHandler.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZonedDateTime;
-
 @ControllerAdvice
 public class GlobalExceptionHandler{
 
     @ExceptionHandler
     public ResponseEntity<?> userAlreadyExistException(RegistrationException registrationException,
                                                        HttpServletRequest httpServletRequest){
-        ApiResponse apiResponse = ApiResponse.builder()
+        com.semicolon.ewallet.Exception.ApiResponse apiResponse = ApiResponse.builder()
                 .timeStamp(ZonedDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .isSuccessful(false)
@@ -35,22 +34,18 @@ public class GlobalExceptionHandler{
                 .path(httpServletRequest.getRequestURI())
                 .data(accountException.getMessage())
                 .build();
-
         return new ResponseEntity<>(apiResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
-    public  ResponseEntity<ApiResponse>GenericHandler(
-            Exception exception,
-            HttpServletRequest httpServletRequest){
-        ApiResponse apiResponse = ApiResponse.builder()
+    public  ResponseEntity<ApiResponse>GenericHandler(Exception exception, HttpServletRequest httpServletRequest){
+        com.semicolon.ewallet.Exception.ApiResponse apiResponse = ApiResponse.builder()
                 .timeStamp(ZonedDateTime.now())
                 .data(exception.getMessage())
                 .path(httpServletRequest.getRequestURI())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .isSuccessful(false)
                 .build();
-        return new ResponseEntity<>(apiResponse,
-                HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 }
